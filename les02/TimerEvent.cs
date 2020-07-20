493ec4059a6a47cfba11f95f29153397d6c617ca
jevlogin <jev.login@mail.ru>
2020-07-20 16:04:10 +0300

    События

---
+++ les02/TimerEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les02
+{
+    class TimerEvent
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            // Здесь мы используем готовый делегат из пространства имен System.Timers
+            // Можно оставить просто название метода. Здесь представлен такой сложный механизм,
+            // чтобы дать понять, что для назначения события используется делегат, который,
+            // в свою очередь, предназначен, чтобы содержать в себе ссылку на метод
+            timer.Elapsed += new ElapsedEventHandler(TimerEventHandler);
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            Console.ReadKey();
+
+        }
+
+        // Все события в Windows Forms строятся по этому шаблону
+        // Сначала идет информация о том, кто создал событие, а потом передается информация о событии
+        // С помощью объекта sender мы можем узнать, кто сгенерировал событие
+        // А с помощью объекта e - информацию о событии
+
+        private static void TimerEventHandler(object sender, ElapsedEventArgs e)
+        {
+            // Выведем информацию о том, что сгенерировало событие
+            Console.WriteLine($"{sender.ToString()}");  //  можно упростить интерполяцию
+
+            //  и информацию, переданную событию
+            //Console.WriteLine($"{e.SignalTime.ToString()}");
+            Console.WriteLine($"{e.SignalTime}");   //  можно упростить интерполяцию
+
+
+        }
+    }
+}
