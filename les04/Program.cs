e8208a338513c5cd195b70991b1de29b894fc9c1
jevlogin <jev.login@mail.ru>
2020-07-20 16:17:52 +0300

    Приложение  "Две формы"

---
+++ les04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace les04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            Form form2 = new Form();
+            form1.Text = "Эта форма запущена с использованием метода Run класса Application";
+            form2.Text = "Это форма для демонстрации возможности создавать несколько форм";
+            form2.Show();
+            Application.Run(form1);
+            MessageBox.Show("Application.Run() вернул " + "управление в метод Main(). До Свидания", "Приложение \"Двеформы\"");
+        }
+    }
+}
