e82e79153a82428e7d80bd3b6fcba2f06d7d7b82
jevlogin <jev.login@mail.ru>
2020-07-20 15:50:48 +0300

    Добавьте файлы проекта.

---
+++ LessonLoginov07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LessonLoginov07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MessageBox простейший способ вывода информации на экран
+            // У метода Show большое количество перезагрузок, с помощью
+            // которых можно управлять видом окна сообщения
+            MessageBox.Show("Сообщение из Windows Forms!");
+            MessageBox.Show("Сообщение из Windows Forms с заголовком", "Заголовок");
+            if (MessageBox.Show("Вы уверены, что хотите продолжить обучение?", "Заголовок 2", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Console.WriteLine("Вы нажали 'Да!'");
+            }
+            else
+            {
+                Console.WriteLine($"Вы нажали 'Нет'");
+            }
+            Console.WriteLine("Можно выводить сообщения в консоль");
+            Console.WriteLine("Не стоит сейчас слишком задерживаться на изучение MessageBox.Show. У вас еще будет такая возможность");
+            
+            Console.ReadKey();
+        }
+    }
+}
