645dc3b0994618a62645e84f2c0fb6222896c755
jevlogin <jev.login@mail.ru>
2020-07-20 16:12:34 +0300

    Это простое графическое окно

---
+++ les03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace les03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем объект класса Form
+            Form form = new Form();
+            // Обращаемся к некоторым свойствам графического окна
+            form.Text = "Это простое графическое окно";
+            form.Width = 1000;
+            // Показываем этот объект
+            form.Show();
+            // Если не сделать паузу, то консольное окно и форма закроются
+
+            Thread.Sleep(3000);
+        }
+    }
+}
