e88cf474466bfd005f129ae7ef2b8cdfb873a3f6
jevlogin <jev.login@mail.ru>
2020-07-22 20:21:14 +0300

    Подчистил код

--- GuessTheNumber/Form1.cs
+++ GuessTheNumber/Form1.cs
@@ -23,23 +23,15 @@ namespace GuessTheNumber
         public Form1()
         {
             InitializeComponent();
-
             CurrentValue = rnd.Next(1, 101);
-
-            Debug.WriteLine(CurrentValue);  //TODO НЕ ЗАБЫТЬ УБРАТЬ!
-
             lblAttempt.Text = tempAttemp + ++CurrentAttemp;
         }
 
-        private void btnMenuItem1Close_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+        private void btnMenuItem1Close_Click(object sender, EventArgs e) => Close();
 
         private void btnMenuItem1NewGame_Click(object sender, EventArgs e)
         {
             CurrentValue = rnd.Next(1, 101);
-            Debug.WriteLine(CurrentValue);  //TODO НЕ ЗАБЫТЬ УБРАТЬ!
             MessageBox.Show("Добро пожаловать в игру!\n\nПравила простые: УГАДАЙ ЧИСЛО, которое загадал компьютер.");
             CurrentAttemp = 1;
             btnOk.Show();
@@ -62,14 +54,8 @@ namespace GuessTheNumber
             }
             else
             {
-                if (tempUserAnswer > CurrentValue)
-                {
-                    MessageBox.Show($"Ваше значение выше задуманного!");
-                }
-                else if (tempUserAnswer < CurrentValue)
-                {
-                    MessageBox.Show($"Ваше значение меньше задуманного!");
-                }
+                if (tempUserAnswer > CurrentValue) MessageBox.Show($"Ваше значение выше задуманного!");
+                else if (tempUserAnswer < CurrentValue) MessageBox.Show($"Ваше значение меньше задуманного!");
                 MessageBox.Show($"С каждым бывает! Попробуйте еще!");
                 lblAttempt.Text = tempAttemp + ++CurrentAttemp;
             }
